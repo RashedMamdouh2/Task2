8afa63920aa1ef686f14afb730d66d51c2ea2d7c
RashedMamdouh2 <rashed.mahmoud02@eng-st.cu.edu.eg>
2025-07-08 21:40:53 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public abstract class Book
+    {
+
+        public  string ISBN { get; set; }
+        public  string Title { get; set; }
+        public  double Price { get; set; }
+        public  DateTime PublishedYear { get; set; }
+    }
+    public class PapperBook : Book
+    {
+        
+    }
+    public class EBook : Book
+    {
+        
+    }
+    public class DemoBook : Book
+    {
+        
+    }
+    public class Inventory
+    {
+        public List<Book> Books { get; set; }
+        public void AddBook() { 
+        
+        
+        }
+        public void RemoveOutdatedBooks() { 
+        
+        
+        }
+        public void BuyABook() { 
+        
+        
+        }
+    }
+}
